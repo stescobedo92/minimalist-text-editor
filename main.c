b3227e12820b3a13856c0f76fa646d455e9fd474
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-02-16 09:13:46 -0600

    Add function to create and read buffer file

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
 void main(int argc, char** argv) {
-
+    FILE* file = fopen(argv[1], "r");
+    char buffer[1024] = {0};
+    fread(buffer, 1024, 1, file);
+    fclose(file);
 }
