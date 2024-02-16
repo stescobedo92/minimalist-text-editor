643d5f41aefbd0a85a28bdd84cfa222c1c3ca267
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-02-16 09:17:41 -0600

    Add logic for edit current line

--- main.c
+++ main.c
@@ -5,4 +5,9 @@ void main(int argc, char** argv) {
     char buffer[1024] = {0};
     fread(buffer, 1024, 1, file);
     fclose(file);
+
+    printf("Contents:\n%s\n", buffer);
+    int current_line = 0;
+    scanf("%d", &current_line);
+    edit_line(buffer, current_line);
 }
